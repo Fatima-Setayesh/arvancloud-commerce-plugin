a473d230fa9298882fd1fd0bf922c4d1be695d2a
Fatima <vantafatima@gmail.com>
2026-08-21 22:53:47 +0330

    chore: setup plugin architecture

---
+++ arvan-reseller/arvan-reseller.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Arvan Reseller
+ * Description: A standalone reseller plugin for Arvan Cloud products.
+ * Version: 1.0.0
+ * Author: Fatima Team
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
